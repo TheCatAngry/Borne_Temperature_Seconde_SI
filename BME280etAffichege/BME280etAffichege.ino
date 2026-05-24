ce411dcd2cb704c34dd3800c853c3282785d59aa
TheCatAngry <The.cat.angry@Gmail.com>
2026-05-25 01:28:17 +0200

    Ajout d'un premier programme qui permet de lire la température du BME280 ainsi que de commencer à afficher qqchose sur l'écran cependant il ne recupere pas la variable temp

---
+++ BME280etAffichege/BME280etAffichege.ino
@@ -0,0 +1,106 @@
+/*
+BME280 I2C Test.ino
+
+This code shows how to record data from the BME280 environmental sensor
+using I2C interface. This file is an example file, part of the Arduino
+BME280 library.
+
+GNU General Public License
+
+Written: Dec 30 2015.
+Last Updated: Oct 07 2017.
+
+Connecting the BME280 Sensor:
+Sensor              ->  Board
+-----------------------------
+Vin (Voltage In)    ->  5V
+Gnd (Ground)        ->  Gnd
+SDA (Serial Data)   ->  A4 
+SCL (Serial Clock)  ->  A5 
+
+ */
+
+#include <BME280I2C.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
+                  // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
+
+//////////////////////////////////////////////////////////////////
+void setup()
+{
+  Serial.begin(9600);
+
+  while(!Serial) {} // Wait
+
+  Wire.begin();
+  
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  
+  while(!bme.begin())
+  {
+    Serial.println("Could not find BME280 sensor!");
+    delay(1000);
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+void loop()
+{
+   printBME280Data(&Serial);
+   testdrawstyles();
+   delay(500);
+
+}
+
+//////////////////////////////////////////////////////////////////
+
+void printBME280Data
+(
+   Stream* client
+)
+{
+   float temp(NAN), hum(NAN), pres(NAN);
+
+   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+
+   bme.read(pres, temp, hum, tempUnit, presUnit);
+
+   client->print("Temp: ");
+   client->print(temp);
+   client->println("°"+ String(tempUnit == BME280::TempUnit_Celsius ? 'C' :'F'));
+
+   delay(1000);
+}
+
+void testdrawstyles(void) {
+  float temp(NAN);
+
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.print("Temp: ");
+  display.print(temp);
+  display.print(" C");
+
+  display.display();
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Borne_Temperature_Seconde_SI
+
+Ceci est un projet personel, qui consiste à créer une borne de température avec un arsduino nano, un affichage grâce à un écran I2C Oled Display et un bme280 comme capteur.
